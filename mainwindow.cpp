47595ee6acd4c929b0158c8af7c791520a5c2566
Max Waine <bobishmax@hotmail.com>
2019-10-28 01:36:46 +0000

    Add wiki opening; refactor out common code

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -16,8 +16,12 @@ MainWindow::~MainWindow()
    delete ui;
 }
 
+//=============================================================================
+//
+// Common event code
+//
 
-void MainWindow::on_pushButton_addIWAD_released()
+void MainWindow::addIWAD()
 {
    const QString fileStr = QFileDialog::getOpenFileName(
       this, tr("Open File"), QString(), tr("DOOM Game Files (*.wad *.iwad *.pke)")
@@ -28,13 +32,13 @@ void MainWindow::on_pushButton_addIWAD_released()
       ui->comboBox_IWAD->addItem(fileStr);
 }
 
-void MainWindow::on_pushButton_removeIWAD_released()
+void MainWindow::removeIWAD()
 {
    if(ui->comboBox_IWAD->count() != 0 && ui->comboBox_IWAD->currentIndex() != -1)
       ui->comboBox_IWAD->removeItem(ui->comboBox_IWAD->currentIndex());
 }
 
-void MainWindow::on_pushButton_addFile_released()
+void MainWindow::addFile()
 {
    const QString fileStr = QFileDialog::getOpenFileName(
       this, tr("Open File"), QString(), tr("DOOM Game Files (*.wad *.pke)")
@@ -42,9 +46,31 @@ void MainWindow::on_pushButton_addFile_released()
    ui->listWidget_files->addItem(fileStr);
 }
 
-void MainWindow::on_pushButton_removeFile_released()
+void MainWindow::removeFile()
 {
    const auto currItem = ui->listWidget_files->currentItem();
    if(currItem != nullptr)
       delete currItem;
 }
+
+void MainWindow::openWiki()
+{
+   QUrl eternityWikiURL("http://eternity.youfailit.net/wiki/Main_Page");
+   QDesktopServices::openUrl(eternityWikiURL);
+}
+
+//=============================================================================
+//
+// Events (slots)
+//
+
+void MainWindow::on_pushButton_addIWAD_released()    { addIWAD(); }
+void MainWindow::on_pushButton_removeIWAD_released() { removeIWAD(); }
+
+void MainWindow::on_pushButton_addFile_released()    { addFile(); }
+void MainWindow::on_action_addFile_triggered()       { addFile(); }
+void MainWindow::on_pushButton_removeFile_released() { removeFile(); }
+void MainWindow::on_action_removeFile_triggered()    { removeFile(); }
+
+void MainWindow::on_pushButton_wiki_released() { openWiki(); }
+void MainWindow::on_actionEternity_Wiki_triggered() { openWiki(); }
--- mainwindow.h
+++ mainwindow.h
@@ -15,14 +15,24 @@ public:
    MainWindow(QWidget *parent = nullptr);
    ~MainWindow();
 
+private:
+   void addIWAD();
+   void removeIWAD();
+   void addFile();
+   void removeFile();
+   void openWiki();
+
 private slots:
    void on_pushButton_addIWAD_released();
-
    void on_pushButton_removeIWAD_released();
 
    void on_pushButton_addFile_released();
-
+   void on_action_addFile_triggered();
    void on_pushButton_removeFile_released();
+   void on_action_removeFile_triggered();
+
+   void on_pushButton_wiki_released();
+   void on_actionEternity_Wiki_triggered();
 
 private:
    Ui::MainWindow *ui;
